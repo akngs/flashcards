6c11b998e683dc20b0c486605cdb9eeb85cd8562
akngs <jania902@gmail.com>
2017-12-31 16:16:44 +0900

    Refactor

--- index.js
+++ index.js
@@ -1,193 +1,125 @@
-var QUIZ_TYPES = [
-  'def2word',
-  'word2def',
-  'sen2word'
-];
+var QUIZ_TYPES = ['def2word', 'word2def', 'sen2word'], deck = [];
 
+d3.csv(
+  'data/words.csv',
+  function (r) {
+    return {
+      id: r.word + '_' + hash([r.wordclass, r.synonyms, r.definition, r.sentences].join('-')),
+      word: r.word,
+      wordclass: r.wordclass,
+      synonyms: r.synonyms.split(';'),
+      definition: r.definition,
+      sentences: r.sentences.split(';').map(function (s) {
+        return s.replace(/\*.+?\*/g, '____');
+      })
+    }
+  },
+  function (err, data) {
+    deck = data;
+    nextQuiz();
+  }
+);
 
-d3.csv('data/words.csv', parseRow, function (err, cards) {
-  nextQuiz(cards);
-});
-
-
-function nextQuiz(cards) {
-  var rightCard = chooseNextCard(cards);
+function nextQuiz() {
   var quizType = QUIZ_TYPES[(Math.random() * QUIZ_TYPES.length) | 0];
-  renderQuiz(quizType, rightCard, cards);
-}
-
-
-function chooseNextCard(cards) {
-  var perfs = loadPerformance();
-  var perfList = [];
-  cards.forEach(function (card) {
-    perfList.push({
-      card: card,
-      perf: perfs[card.id] || {word: card.word, score: 0.5, lastExposedTime: 0}
-    });
-  });
-
-  var now = Date.now();
-  var candidates = perfList
-    .sort(function (a, b) {
-      return d3.ascending(
-        a.perf.score - Math.log(now - a.perf.lastExposedTime + 1) * 0.001,
-        b.perf.score - Math.log(now - b.perf.lastExposedTime + 1) * 0.001
-      );
-    })
-    .slice(0, 10);
-
-  return d3.shuffle(candidates)[0].card;
-}
-
-function renderQuiz(quizType, rightCard, cards) {
-  var wrongCandidates = findWrongCandidates(rightCard, cards).slice(0, 3);
-  var candidates = d3.shuffle([rightCard].concat(wrongCandidates));
+  var card = chooseCard();
+  var choices = d3.shuffle([card].concat(getProbableChoices(card)));
 
   d3.select('.quiz').attr('class', 'quiz ' + quizType);
-  d3.select('.quiz .question').datum(rightCard).each(renderCard);
-  d3.select('.quiz .candidates')
+  d3.select('.quiz .question').datum(card).each(renderCard);
+  d3.select('.quiz .choices')
     .html('')
-    .selectAll('li.card').data(candidates)
-    .enter()
-    .append('li')
-    .classed('card', true)
-    .classed('wrong', false)
+    .selectAll('li.card').data(choices).enter().append('li')
+    .attr('class', 'card')
     .each(renderCard)
-    .on('click', function (c) {
-      onClick(this, cards, c, rightCard);
+    .on('click', function (clickedCard) {
+      if (d3.select(this).classed('wrong')) return;
+      updatePerf(card, clickedCard);
+      var correct = card === clickedCard;
+      d3.select(this).classed(correct ? 'right' : 'wrong', true);
+      if (correct) window.setTimeout(nextQuiz, 500);
     });
 }
 
+function chooseCard() {
+  var perfs = loadPerformance();
+  var now = Date.now();
+
+  function order(p) {
+    return p.score - Math.log(now - p.lastExposedTime) * 0.001;
+  }
+
+  return deck
+    .sort(function (a, b) {
+      return d3.ascending(
+        order(perfs[a.id] || defaultPerfEntry(a)),
+        order(perfs[b.id] || defaultPerfEntry(b))
+      );
+    })
+    .slice(0, 10)[(Math.random() * 10) | 0];
+}
 
 function renderCard(card) {
   var cardSel = d3.select(this)
     .attr('id', 'card' + card.id)
     .attr('data-id', card.id)
     .html('');
-  cardSel.append('p')
-    .attr('class', 'word')
-    .text(card.word);
-  cardSel.append('p')
-    .attr('class', 'definition')
-    .text(card.definition);
-  cardSel.append('ul')
-    .attr('class', 'synonyms')
-    .selectAll('li')
-    .data(d3.shuffle(card.synonyms))
-    .enter()
-    .append('li')
+  cardSel.append('p').attr('class', 'word').text(card.word);
+  cardSel.append('p').attr('class', 'definition').text(card.definition);
+  cardSel.append('ul').attr('class', 'synonyms')
+    .selectAll('li').data(d3.shuffle(card.synonyms)).enter().append('li')
     .text(String);
-  cardSel.append('ul')
-    .attr('class', 'sentences')
-    .selectAll('li')
-    .data(d3.shuffle(card.sentences))
-    .enter()
-    .append('li')
+  cardSel.append('ul').attr('class', 'sentences')
+    .selectAll('li').data(d3.shuffle(card.sentences)).enter().append('li')
     .text(String);
 }
-
-
-function onClick(element, cards, clickedCard, rightCard) {
-  if (d3.select(element).classed('wrong')) return;
-
-  updatePerf(rightCard, clickedCard);
-  if (rightCard === clickedCard) {
-    d3.select(element).classed('right', true);
-    window.setTimeout(function () {
-      nextQuiz(cards);
-    }, 500);
-  } else {
-    d3.select(element).classed('wrong', true);
-  }
-}
-
 
 function updatePerf(rightCard, choosenCard) {
   var perfs = loadPerformance();
-
   var now = Date.now();
   var correct = rightCard === choosenCard;
   updatePerfForEntry(perfs, rightCard, correct, now);
-  if (!correct) {
-    updatePerfForEntry(perfs, choosenCard, correct, now);
-  }
-
+  if (!correct) updatePerfForEntry(perfs, choosenCard, correct, now);
   savePerformance(perfs);
 }
 
-
 function updatePerfForEntry(perfs, card, correct, now) {
-  if (!perfs[card.id]) {
-    perfs[card.id] = {
-      word: card.word,
-      score: 0.5,
-      lastExposedTime: 0
-    };
-  }
-
-  var perf = perfs[card.id];
+  // update performance using exponential-smoothing
+  var perf = perfs[card.id] || defaultPerfEntry(card);
   perf.word = card.word;
   perf.score = perf.score * 0.9 + (correct ? 1 : 0) * 0.1;
   perf.lastExposedTime = now;
   perfs[card.id] = perf;
 }
 
-
-function loadPerformance() {
-  var perfs = JSON.parse(localStorage.getItem('perfs'));
-  if (!perfs) {
-    localStorage.setItem('perfs', '{}');
-    return {};
-  } else {
-    return perfs;
-  }
+function defaultPerfEntry(card) {
+  return {word: card.word, score: 0.5, lastExposedTime: 0};
 }
 
+function loadPerformance() {
+  return JSON.parse(localStorage.getItem('perfs') || '{}');
+}
 
 function savePerformance(perfs) {
   localStorage.setItem('perfs', JSON.stringify(perfs));
 }
 
-
-function parseRow(row) {
-  return {
-    id: row.word + '_' + hash([row.wordclass, row.synonyms, row.definition, row.sentences].join('-')),
-    word: row.word,
-    wordclass: row.wordclass,
-    synonyms: row.synonyms.split(';'),
-    definition: row.definition,
-    sentences: row.sentences.split(';').map(function (s) {
-      return s.replace(/\*.+?\*/g, '____');
-    })
-  }
-}
-
-
-function findWrongCandidates(rightCard, cards) {
-  var sameClasses = cards
+function getProbableChoices(card) {
+  var duplicates = d3.set([card.word]);
+  return d3.shuffle(deck)
     .filter(function (c) {
-      return c.wordclass === rightCard.wordclass;
+      return c.wordclass === card.wordclass;
     })
     .filter(function (c) {
-      return c.word !== rightCard.word;
-    });
-
-  // shuffle and remove duplicated words
-  var words = d3.set();
-  return d3.shuffle(sameClasses).filter(function (c) {
-    var duplicated = words.has(c.word);
-    words.add(c.word);
-    return !duplicated;
-  });
+      var duplicated = duplicates.has(c.word);
+      duplicates.add(c.word);
+      return !duplicated;
+    })
+    .slice(0, 3);
 }
-
 
 function hash(s) {
-  var hash = 5381;
-  var i = s.length;
-  while(i) {
-    hash = (hash * 33) ^ s.charCodeAt(--i);
-  }
+  var hash = 5381, i = s.length;
+  while (i) hash = (hash * 33) ^ s.charCodeAt(--i);
   return hash >>> 0;
 }
